60268eae0cbf51622d96c0b616623da6ad764450
Utshant Gurung <utshantgurung@gmail.com>
2021-05-22 13:23:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [“Amazon”, “Google”, “Facebook”, “Twitter”]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift element
 end
 
 def remove_element_from_end_of_array(array)

b71d9539db3f3fce56636a6408f769b811293624
Utshant Gurung <utshantgurung@gmail.com>
2021-05-22 13:29:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [“Amazon”, “Google”, “Facebook”, “Twitter”]
+  [Amazon, Google, Facebook, Twitter]
 end
 
 def add_element_to_end_of_array(array, element)
